b865e788de7ab7d203e2e8149c9acdba9b9cb7e5
James Monks <jamesmonks131@gmail.com>
2021-04-26 23:15:14 +1000

    Create timer functions

---
+++ R/timer.R
@@ -0,0 +1,60 @@
+#' Format centi-seconds for timer
+#'
+#' @param seconds
+#'
+#' @return
+#'
+#' @examples
+seconds_to_timer <- function(seconds) {
+  minutes <- floor(cs / 60) %>%
+    stringr::str_pad(2, pad = '0')
+  seconds <- floor((cs %% 60)) %>%
+    stringr::str_pad(2, pad = '0')
+  glue::glue('{minutes}:{seconds}')
+}
+
+
+#' Internal Timer
+#'
+#' @return
+#'
+#' @examples
+timer <- function () {
+  .id <<- .id + 1
+  message('\r', seconds_to_timer(.id), appendLF = FALSE)
+}
+
+#' Start the timer
+#'
+#' @param initial
+#' @param delay
+#'
+#' @return
+#' @export
+#'
+#' @examples
+start_timer <- function(initial = 0, delay = 0){
+  if(delay != 0) {
+    later::later(
+      function(){
+        .id <<- initial
+        tcltk2::tclTaskSchedule(1000, timer(), id = "timer", redo = TRUE)
+      },
+      delay = delay
+    )
+  } else {
+    .id <<- initial
+    tcltk2::tclTaskSchedule(1000, timer(), id = "timer", redo = TRUE)
+  }
+
+}
+
+#' Stop the timer
+#'
+#' @return
+#' @export
+#'
+#' @examples
+stop_timer <- function(){
+  tcltk2::tclTaskDelete('timer')
+}
